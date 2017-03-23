1e81ab5b70004e2b1ccbcdb426b7541aca816490
Biffyn <hello@jamesrichards.info>
2017-03-23 01:53:37 +0100

    Added user model and register route

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-const express     = require('express');
-const path        = require('path');
-const bodyParser  = require('body-parser');
-const cors        = require('cors');
-const passport    = require('passport');
-const mongoose    = require('mongoose');
-const config      = require('./config/database');
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const config = require('./config/database');
 
 // Connect to db
 mongoose.connect(config.database);
 
 // On Connection to db
 mongoose.connection.on('connected', () => {
-  console.log('Connected to the database '+config.database);
+    console.log('Connected to the database ' + config.database);
 });
 
 // On Error
 mongoose.connection.on('error', (err) => {
-  console.log('Database Error: '+err);
+    console.log('Database Error: ' + err);
 });
 
 const app = express();
@@ -39,10 +39,10 @@ app.use('/users', users);
 
 // INDEX Route
 app.get('/', (req, res) => {
-  res.send('Invalid Endpoint');
+    res.send('Invalid Endpoint');
 });
 
 // Start Server
 app.listen(port, () => {
-  console.log('Server started on port '+port)
+    console.log('Server started on port ' + port)
 });
--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,41 @@
 const express = require('express');
+const User = require('../models/user');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
 const router = express.Router();
 
 // Register
-router.get('/register', (req, res, next) => {
-  res.send('Register Route');
+router.post('/register', (req, res, next) => {
+    let newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    User.addUser(newUser, (err, user) => {
+        if (err) {
+            res.json({
+                success: false,
+                msg: 'Failed to register user!'
+            });
+        } else {
+            res.json({
+                success: true,
+                msg: 'Successfully registered user!'
+            });
+        }
+    });
 });
 
 // Auth
 router.post('/authenticate', (req, res, next) => {
-  res.send('Authenticate Route');
+    res.send('Authenticate Route');
 });
 
 // Profile
 router.get('/profile', (req, res, next) => {
-  res.send('Profile Route');
+    res.send('Profile Route');
 });
 
 module.exports = router;
